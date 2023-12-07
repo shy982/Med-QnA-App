66cd98cc7d57009c7f8da2abd22b7f940614d08f
Shyam Renjith <shyamrenjith@Shyams-MacBook-Air.local>
2023-12-06 17:33:41 -0800

    Refactoring the backend

--- src/main/backend/marshaler/chat_io_service.py
+++ src/main/backend/marshaler/chat_io_service.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import time
+import os
 import openai
 from dotenv import load_dotenv
-from src.main.backend.qna_service.openai_client import request_gpt_turbo
+from qna_service.openai_client import request_gpt_turbo
+from qna_service.openai_client import run_rag_pipeline
+
+
 app = Flask(__name__)
 CORS(app)
+
+# TODO: Maintain conversation i.e. previous chats to be taken into account
+# TODO: API to process uploaded document text and consider as context.
 
 @app.route('/v1/chat/simple', methods=['POST'])
 def process_message_simple_test():
@@ -21,17 +28,14 @@ def process_message_simple_test():
 def process_message_with_chatgpt():
     data = request.json
     messages = data.get('messages', [])
-    load_dotenv()
     response = request_gpt_turbo(messages)
-    client = openai.OpenAI()
     return response
 
 @app.route('/v1/chat/rag', methods=['POST'])
 def process_message_with_rag_chatgpt():
     data = request.json
     messages = data.get('messages', [])
-    qry = messages[-1]['content']
-    answer = run_rag_pipeline(qry)
+    answer = run_rag_pipeline(messages)
     return answer.strip()
 
 @app.route('/v2/chat/rag', methods=['POST'])
@@ -39,56 +43,6 @@ def process_message_with_rag_llama():
     data = request.json
     # TBD 
     return None
-
-def run_rag_pipeline(query, model="gpt-3.5-turbo-instruct", dataset="nfcorpus"):
-    # Imports
-    from dotenv import load_dotenv
-    from langchain.embeddings import OpenAIEmbeddings
-    from langchain.llms import OpenAI
-    from langchain.prompts import ChatPromptTemplate
-    from langchain.schema.output_parser import StrOutputParser
-    from langchain.schema.runnable import RunnablePassthrough
-    from langchain.vectorstores import FAISS
-    
-    import os
-    import pickle
-
-    
-    # Load index from file
-    load_dotenv()
-
-    loaded_faiss_vs = FAISS.load_local(
-        folder_path=f"../../experiments/vectordb/faiss/{dataset}/",
-        embeddings=OpenAIEmbeddings())
-    retriever = loaded_faiss_vs.as_retriever(search_kwargs={"k": 5})
-
-    # Define the RAG pipeline
-    llm = OpenAI(model_name=model, openai_api_key=os.getenv("OPENAI_API_KEY"))
-
-    template = """Answer the question or Explain the topic given this additional context: {context}
-    Question: {question}"""
-    prompt = ChatPromptTemplate.from_template(template)
-
-    docs_file_path = f"../../experiments/dataset/{dataset}/documents.pkl"
-    with open(docs_file_path, "rb") as file:
-        docs = pickle.load(file)
-
-    def format_docs(_docs):
-        ls = []
-        for doc in _docs:
-            if doc.page_content in docs:
-                ls.append(docs[doc.page_content]["text"][:800])
-        return ls
-    
-    chain = ({"context": retriever | format_docs, "question": RunnablePassthrough()} 
-             | prompt 
-             | llm 
-             | StrOutputParser())
-
-    # Run the RAG pipeline
-    response = chain.invoke(query)
-
-    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
